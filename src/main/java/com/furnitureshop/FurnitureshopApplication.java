8ce08f09ef4c568c9ead8a391c0ed1d25b77510e
Gabriela Ivanova <62185813+slynchoO@users.noreply.github.com>
2023-11-16 15:25:13 +0200

    first upload

---
+++ src/main/java/com/furnitureshop/FurnitureshopApplication.java
@@ -0,0 +1,13 @@
+package com.furnitureshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FurnitureshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/furnitureshop/FurnitureshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furnitureshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
